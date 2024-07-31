2dda58846353f11af0f4f16bd7f5767079b200f6
yasir707 <121334884+yasir707@users.noreply.github.com>
2024-07-31 17:50:51 +0500

    Delete final-project directory

--- final-project/server/connection.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose')
-main().then(()=>console.log('DB connected')).catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/socialmedia');
-}
-
-let connection = mongoose.connection;
-module.exports=connection;
