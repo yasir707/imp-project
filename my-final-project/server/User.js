50479e8a64dd4b91c02153c45eb60a145c6ec035
yasir707 <121334884+yasir707@users.noreply.github.com>
2024-07-31 17:50:37 +0500

    Delete my-final-project directory

--- my-final-project/server/User.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password: String, 
-  });
-
-  const User = mongoose.model('User', userSchema);
-  module.exports=User;
